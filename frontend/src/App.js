9d659316b595e5326b12499ca5d0ded0a7f29422
ChunxuYang <yangchunxu@pku.edu.cn>
2021-03-31 22:49:27 +0800

    hello world

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Typist from "react-typist";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p></p>
+          <Typist
+            cursor={{
+              show: true,
+              blink: true,
+              element: "I",
+              hideWhenDone: true,
+              hideWhenDoneDelay: 500,
+            }}
+          >
+            Hello world from our software-engineering program!
+          </Typist>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
