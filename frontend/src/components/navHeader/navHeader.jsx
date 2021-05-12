edc9be119688e338924f95774c999ca4aee1a70a
ChunxuYang <yangchunxu@pku.edu.cn>
2021-05-12 19:30:38 +0800

    simplify imports

--- frontend/src/components/navHeader/navHeader.jsx
+++ frontend/src/components/navHeader/navHeader.jsx
@@ -8,7 +8,6 @@ import InputBase from "@material-ui/core/InputBase";
 import Badge from "@material-ui/core/Badge";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
-import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import MailIcon from "@material-ui/icons/Mail";
