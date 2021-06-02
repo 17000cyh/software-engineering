a5985094c267eb5cf5f03aecb70f304a8c7de1cb
17000cyh <56074168+17000cyh@users.noreply.github.com>
2021-06-02 15:30:02 +0800

    Add files via upload

--- database/db_interface/db_function.py
+++ database/db_interface/db_function.py
@@ -158,3 +158,55 @@ def insert_message(user_id,target_user_id,content,time):
     :param content:
     :return:None
     """
+
+def get_like(user_id):
+    """
+    这个函数通过user_id获取点赞内容。点赞的内容是一个列表，列表当中是一个like字典，如下：
+    {'total','last_like_user','content','like_id'}
+    其中，total是指这一个内容（比如说，评论、回复、文章等等）一共有几个人点赞
+    last_like_user记录了最后一个点赞用户的名称
+    content是被点赞内容（评论、回复、文章等）的一个摘要，比如前十个字
+    like_id是被点赞内容的id，以便于找到这个点赞的具体内容
+    :param user_id:
+    :return:like_list
+    """
+
+def get_reply_list(user_id):
+    """
+    这个函数根据user_id获取新增的回复内容。新增的回复内容是一个列表，列表当中是一个reply字典，如下：
+    {'user_name':user_name,'reply_id':reply_id,'type':type,'content':content}
+    其中，user_name表示作出回复的这个用户的名称，reply_id指示了当前reply的id
+     注意在该函数的语境下，reply既可以指对自己文章的一个comment，也可以指对于comment的reply
+    所以，type表示目标内容的类型有两种：comment和reply，content是对reply内容的一个概括（比如说，前几个字）
+    :param user_id:
+    :return:reply_list
+    """
+
+def get_collection_list(user_id):
+    """
+    这个函数根据user_id获取用户最近的收藏内容。收藏内容是一个列表，其中每一个列表当中都是一个infor字典，结构如下：
+    {'good_id':good_id,'good_name':good_name,'good_type':good_type}
+    :param user_id:
+    :return:collection_list
+    """
+
+def get_history_list(user_id):
+    """
+    这个函数的输入和输出形式和上一个函数完全一样，只不过数据访问是从用户的history当中来的
+    :param user_id:
+    :return:history_list
+    """
+
+def get_comment_infor(target_id):
+    """
+    这个函数根据target_id对于某一篇文章/商品的评论内容进行返回，返回的内容是一个comment_list，list当中的comment如下
+    {'content':content,'username':username,'user_id':user_id,'id':id,'profile_path':profile_path,'time':time,'like_number':like_number,
+    'reply_list':reply_list},
+    其中，content是指本comment的内容，username是指发送这个评论的用户名称，user_id指发送评论的用户的id，id指评论的id
+    profile_path指用户头像的路径，time指评论发送的时间，like_number指点赞的数量，reply_list指针对这个评论的回复列表，它也是一个列表，每一项的具体结构如下：
+    {'content':content,'username':username,'user_id':user_id,'id':id,'target_user':target_user,'target_id':target_id,'reply_type':reply_type':reply_type,'profile_path':profile_path,'time':time,'like_number':like_number},
+    由于reply可以是针对comment的，也可以是针对某一个reply的，所以这里使用了reply_type进行区分reply_type有两个值：reply和comment
+    除此之外，这里还有几个参数：target_user是指这条reply回复的用户，target_id是指回复用户的id
+    :param target_id:
+    :return:comment_list
+    """
